add4c9bac28e3bc8cf2c17436cd42db9f43d4922
Dylan Middendorf <middendm@miamioh.edu>
2024-05-03 10:59:34 -0400

    Implemented Random Search w/ SVM

--- models/knn.py
+++ models/knn.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn.model_selection import GridSearchCV
 import time
 from utils import train_test_dataframes
 
@@ -28,15 +28,12 @@ def grid_search_cv() -> None:
         "p": [1, 2],
     }
 
-    start = time.time()
     knn = KNeighborsClassifier()
     clf = GridSearchCV(knn, param_grid=param_grid, verbose=2, n_jobs=5)
-    # print(cross_val_score(knn, X_train, y_train, n_jobs=5, verbose=1))
     clf.fit(X_train, y_train)
 
     results_df = pd.DataFrame(clf.cv_results_)
     results_df.to_csv("./results/knn_search.csv")
-    print(time.time() - start)
 
 
 def main():
--- models/svm.py
+++ models/svm.py
@@ -1,22 +1,50 @@
+import pandas as pd
+import numpy as np
+
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
+from sklearn.model_selection import RandomizedSearchCV
 
 from utils import train_test_dataframes
 
-def main():
-    train, test = train_test_dataframes("./datasets")
-    
-    # Subsample the training dataset to reduce computational overhead
-    train = train.sample(10_000)
-    
-    X_train, y_train = train.drop("label", axis=1), train["label"]
-    X_test, y_test = test.drop("label", axis=1), test["label"]
 
-    clf = SVC(cache_size=60000)
+def evaluate_model(clf: SVC) -> float:
+    train, test = train_test_dataframes("./datasets")
+    train = train.sample(10_000)  # Reduce computational overhead
+
+    X_train, y_train = (train.drop("label", axis=1) / 255.0), train["label"]
+    X_test, y_test = (test.drop("label", axis=1) / 255.0), test["label"]
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)  # Can be used for additional metrics
+    return accuracy_score(y_test, y_pred)
+
+
+def random_search_cv() -> None:
+    train, test = train_test_dataframes("./datasets")
+    train = train.sample(10_000)  # Reduce computational overhead
+
+    X_train, y_train = (train.drop("label", axis=1) / 255.0), train["label"]
+    parameters = {
+        "C": np.logspace(-3, 2, 6),
+        "kernel": ["linear", "poly", "rbf"],
+        "degree": np.arange(1, 5 + 1, 1).tolist(),
+        "gamma": np.logspace(-3, 2, 6),
+        "coef0": np.arange(0.0, 10.0, 0.1).tolist(),
+        "tol": np.arange(0.001, 0.01 + 0.001, 0.001).tolist(),
+        "cache_size": [60_000],
+    }
+
+    clf = RandomizedSearchCV(SVC(), param_distributions=parameters, verbose=2, n_jobs=5)
     clf.fit(X_train, y_train)
 
-    y_pred = clf.predict(X_test)
-    print("Accuracy:", accuracy_score(y_test, y_pred))
+    results_df = pd.DataFrame(clf.cv_results_)
+    results_df.to_csv("./results/svc_search_2.csv")
+
+
+def main():    
+    clf = SVC()
+    print(evaluate_model(clf))
 
 
 if __name__ == "__main__":
